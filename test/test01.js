d530900df86d28cc3f010ce53b583de6f4a5948d
David <d.pitarch@outlook.es>
2021-03-25 09:15:33 +0100

    Edit tiers function, first count the spaces must have the floor and after print the symbol until make the structure we want show

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,24 @@
 function tiers(index) {
-	return "";
+	
+	let result = '';
+
+    for (let i = 0; i < index; i ++) {
+        
+		let floor = '';
+
+        // Bucle para rellenar los espacios de la piramide
+        for (let j = 1; j < index - i; j ++) {
+            floor = floor + ' ';
+        }
+
+        // Bucle para pintar los asteriscos
+        for (let j = 0; j < i + 1; j ++) {
+            floor = floor + '*';
+        }
+
+        result += floor + '\n';
+    }
+
+    return result;
 }
 module.exports = tiers;
